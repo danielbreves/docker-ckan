f824d2ff0b3997bfee131869e99fe4241225dae2
filip.mihajlovski <filip.mihajlovski@keitaro.com>
2023-02-09 12:09:37 +0100

    refactored variables for dp+

--- images/datapusher+/setup/datapusher_settings.py
+++ images/datapusher+/setup/datapusher_settings.py
@@ -19,12 +19,12 @@ import os
 
 
 # turns on logger at Debug level
-DEBUG = False
+DEBUG = bool(os.environ.get('DATAPUSHER_DEBUG_MODE', 'False'))
 # If False, configures the logger for production
 # i.e. logs to STDERR and LOG_FILE (autorotates after 68mb, with 5 backups),
 # and emails errors to admins.
 # If True, only turns on Debug if DEBUG = True
-TESTING = False
+TESTING = bool(os.environ.get('DATAPUSHER_TESTING', 'False'))
 SECRET_KEY = str(uuid.uuid4())
 USERNAME = str(uuid.uuid4())
 PASSWORD = str(uuid.uuid4())
@@ -32,12 +32,21 @@ PASSWORD = str(uuid.uuid4())
 NAME = 'datapusher-plus'
 
 # The connect string of the Datapusher+ Job database
-
+## ASK TOME
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/job_store.db'
 
 # webserver host and port
+DB_HOST = os.environ.get('DBHost')
+DATASTORE_DBNAME = os.environ.get('DatastoreDBName')
+DATASTORE_RW_DBUSER = os.environ.get('DatastoreRWDBUser')
+DATASTORE_RW_DBPASS = os.environ.get('DatastoreRWDBPass')
+# ASK BLAZ
+# 
+# CHART_WRITE_ENGINE_URL is created using the helm chart variables
+if DB_HOST != None and DATASTORE_DBNAME != None and DATASTORE_RW_DBUSER != None and DATASTORE_RW_DBPASS != None :
+    CHART_WRITE_ENGINE_URL = 'postgresql://{DATASTORE_RW_DBUSER}:{DATASTORE_RW_DBPASS}@{DB_HOST}/{DATASTORE_DBNAME}'
 
-WRITE_ENGINE_URL = 'postgresql://datapusher:YOURPASSWORD@localhost/datastore_default'
+WRITE_ENGINE_URL = os.environ.get('DATAPUSHER_WRITE_ENGINE_URL') or CHART_WRITE_ENGINE_URL or 'postgresql://datastorerw:pass@postgres/datastore_default'
 HOST = '0.0.0.0'
 PORT = 8000
 
@@ -68,9 +77,9 @@ REWRITE_RESOURCES = os.environ.get('DATAPUSHER_REWRITE_RESOURCES', False)
 REWRITE_URL = os.environ.get('DATAPUSHER_REWRITE_URL', 'http://ckan:5000/')
 # If this is not zero, the number of preview rows to push into the datastore
 # If zero, it pushes the entire file
-PREVIEW_ROWS = 0
+PREVIEW_ROWS = int(os.environ.get('DATAPUSHER_PREVIEW_ROWS', 0))
 # If this is True, only the first n PREVIEW_ROWS are downloaded, and not the whole file
-DOWNLOAD_PREVIEW_ONLY = False
+DOWNLOAD_PREVIEW_ONLY = bool(os.environ.get('DATAPUSHER_DOWNLOAD_PREVIEW_ONLY', False))
 # number of days to keep job history
 KEEP_JOBS_AGE = 60
 
@@ -90,45 +99,45 @@ FILE_BIN = '/usr/bin/file'
 
 # Dates are parsed with an MDY preference by default
 # set PREFER_DMY = True if date-parsing should prefer DMY instead
-PREFER_DMY = False
+PREFER_DMY = True
 
 # The zero-based index of the default sheet to export to CSV. 0 is the first sheet.
 # Accepts negative numbers. -1 is the last sheet, -2 the 2nd to last sheet, etc.
-DEFAULT_EXCEL_SHEET = 0
+DEFAULT_EXCEL_SHEET = int(os.environ.get('DATAPUSHER_DEFAULT_EXCEL_SHEET', 0))
 
 # Check if a file is sorted and has duplicates
-SORT_AND_DUPE_CHECK = True
+SORT_AND_DUPE_CHECK = bool(os.environ.get('DATAPUSHER_SORT_AND_DUPE_CHECK', True))
 
 # Should CSVs be deduped? Note that deduping also
 # sorts the CSV.
-DEDUP = False
+DEDUP = bool(os.environ.get('DATAPUSHER_DEDUP', False))
 
 # -------- SUMMARY STATS SETTINGS -----------
 # Create a resource for calculated summary stats?
-ADD_SUMMARY_STATS_RESOURCE = False
+ADD_SUMMARY_STATS_RESOURCE = bool(os.environ.get('DATAPUSHER_ADD_SUMMARY_STATS_RESOURCE', False))
 
 # Summary Stats don't make sense if PREVIEW_ROWS > 0
 # because, you're only summarizing the preview, not the whole file
 # Set to True if Summary Stats should also be done for previews
-SUMMARY_STATS_WITH_PREVIEW = False
+SUMMARY_STATS_WITH_PREVIEW = bool(os.environ.get('DATAPUSHER_SUMMARY_STATS_WITH_PREVIEW', False))
 
 # additional command line options to pass to qsv stats when creating
 # summary stats. Set to `--everything` if you want to include all the stats,
 # particularly, when ADD_SUMMARY_STATS_RESOURCE is True
-SUMMARY_STATS_OPTIONS = ''
+SUMMARY_STATS_OPTIONS = os.environ.get('DATAPUSHER_SUMMARY_STATS_OPTIONS', '')
 
 # -------- AUTO INDEX SETTINGS ----------
 # if AUTO_INDEX_THRESHOLD > 0 or AUTO_INDEX_DATES is true
 # create indices automatically based on as column's cardinality (number of unique values)
 #   - if a column's cardinality <= AUTO_INDEX_THRESHOLD, create an index for that column
 #   - if AUTO_INDEX_THRESHOLD = -1, index all columns regardless of its cardinality
-AUTO_INDEX_THRESHOLD = 3
+AUTO_INDEX_THRESHOLD = int(os.environ.get('DATAPUSHER_AUTO_INDEX_THRESHOLD', 3))
 
 # for columns w/ cardinality equal to record_count, it's all unique values, create a unique index
-AUTO_UNIQUE_INDEX = True
+AUTO_UNIQUE_INDEX = bool(os.environ.get('DATAPUSHER_AUTO_UNIQUE_INDEX', True))
 
 # always index date fields?
-AUTO_INDEX_DATES = True
+AUTO_INDEX_DATES = bool(os.environ.get('DATAPUSHER_AUTO_INDEX_DATES', True))
 
 # ------ AUTO ALIAS SETTINGS ----------
 # Should an alias be automatically created?
@@ -137,26 +146,26 @@ AUTO_INDEX_DATES = True
 # the CKAN Datastore database.
 # Aliases are created by concatenating "{resource_name}-{package_name}-{owner_org_name}"
 # truncated at 55-characters.
-AUTO_ALIAS = False
+AUTO_ALIAS = bool(os.environ.get('DATAPUSHER_AUTO_ALIAS', False))
 
 # Should aliases should always be unique? In case of an alias name collision, a three-digit
 # sequence number is appended.
-AUTO_ALIAS_UNIQUE = False
+AUTO_ALIAS_UNIQUE = bool(os.environ.get('DATAPUSHER_AUTO_ALIAS_UNIQUE', False))
 
 # -------- PII SETTINGS -----------
-PII_SCREENING = False
+PII_SCREENING = bool(os.environ.get('DATAPUSHER_PII_SCREENING', False))
 
 # Stop scanning on first PII found
-PII_QUICK_SCREEN = False
+PII_QUICK_SCREEN = bool(os.environ.get('DATAPUSHER_PII_QUICK_SCREEN', False))
 
 # Abort Datapusher+ job if PII is found
-PII_FOUND_ABORT = True
+PII_FOUND_ABORT = bool(os.environ.get('DATAPUSHER_PII_FOUND_ABORT', True))
 
 # Create a resource where PII candidates are stored?
-PII_SHOW_CANDIDATES = True
+PII_SHOW_CANDIDATES = bool(os.environ.get('DATAPUSHER_PII_SHOW_CANDIDATES', True))
 
 # The resource ID/alias of a Text file that has the 
 # regex patterns to use for PII scanning.
 # If this is not specified, the default PII scanning rules in
 # default_pii_regexes.txt are used
-PII_REGEX_RESOURCE_ID_OR_ALIAS = ''
+PII_REGEX_RESOURCE_ID_OR_ALIAS = os.environ.get('DATAPUSHER_PII_REGEX_RESOURCE_ID_OR_ALIAS', '')
